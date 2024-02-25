44293bd7a6cdbacc20ab54e9dab9997e12bc776c
SurajBhari <surajbhari159@gmail.com>
2024-02-25 11:44:20 +0530

    fix logic for deciding what file to delete

--- app.py
+++ app.py
@@ -10,27 +10,27 @@ app = Flask(__name__)
 
 @app.route("/download/<vid>/<start_time>/<end_time>")
 def download(vid, start_time, end_time):
-  output_filename = vid+"_"+start_time+"_"+end_time
-  video_url = "https://youtube.com/watch?v="+vid
-  start = int(start_time)
-  end = int(end_time)
-  print(video_url)
-  print(yt_dlp.version.__version__)
-  params = {
-        'download_ranges': yt_dlp.utils.download_range_func([], [[start, end]]),
-        'match_filter': yt_dlp.utils.match_filter_func("!is_live & live_status!=is_upcoming & availability=public"),
-        'no_warnings': True,
-        'noprogress': True,
-        'outtmpl': {'default': output_filename},
-        'quiet': True
-    }
-  with yt_dlp.YoutubeDL(params) as ydl:
-      try:
-          ydl.download([video_url])
-      except yt_dlp.utils.DownloadError as e:
-          return str(e)
-  return send_file([x for x in os.listdir(".") if x.startswith(output_filename)][0])
+	start = int(float(start_time))
+	end = int(float(end_time))
+	output_filename = vid+"_"+str(start)+"_"+str(end)
+	matching_files = [x for x in os.listdir(".") if x.startswith(output_filename) and not x.endswith("part")]
+	if matching_files:
+		return send_file(matching_files[0])
+	[os.remove(x) for x in os.listdir(".") if not os.path.isdir(x) and x.count("_")]
+	video_url = "https://youtube.com/watch?v="+vid
+	params = {
+			'download_ranges': yt_dlp.utils.download_range_func([], [[start, end]]),
+			'match_filter': yt_dlp.utils.match_filter_func("!is_live & live_status!=is_upcoming & availability=public"),
+			'outtmpl': {'default': output_filename},
+		}
+	with yt_dlp.YoutubeDL(params) as ydl:
+		try:
+			ydl.download([video_url])
+		except yt_dlp.utils.DownloadError as e:
+			return str(e)
+
+	return send_file([x for x in os.listdir(".") if x.startswith(output_filename)][0])
 
 @app.route("/")
 def index():
-  return "Yes."
+	return "Yes."
